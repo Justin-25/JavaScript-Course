3c8f6c45eba5f4337dda0a3568f9d2c22e3cf0a5
unknown <justinmandia25@gmail.com>
2026-03-06 22:11:57 +0800

    Challeng Exercise

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,28 @@
 console.log(document.querySelector('.test').classList.contains('js-button'));
 
 function istoggled(button) {
-
   if (button.classList.contains('is-toggled')) {
     button.classList.remove('is-toggled');
   } else {
     button.classList.add('is-toggled');
   }
+}
+
+function istoggled(button) {
+  const defaultBtn = document.querySelectorAll('.toggled');
+
+  // for - This creates a loop that will repeat an action multiple times
+  // let i = 0 - Start counting from 0 (first button)
+  // i < defaultBtn.length - Keep looping as long as i is less than the total number of buttons
+  // i++ - After each loop, add 1 to i (move to the next button)
+  // defaultBtn[i] - Get the button at position i (0 = first button, 1 = second button, etc.)
+  // .classList.remove('is-toggled') - Remove the 'is-toggled' class from that button
+  for (let i = 0; i < defaultBtn.length; i++) {
+    defaultBtn[i].classList.remove('is-toggled')
+  }
+
+
+  button.classList.add('is-toggled');
 }
 
 function myMusicToggled() {
