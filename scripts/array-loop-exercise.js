85bcd5d65cb8682a0625071868a942eb9ff0e260
unknown <justinmandia25@gmail.com>
2026-03-10 17:06:00 +0800

    Done Challenged of lecture in arrays and loop

--- scripts/array-loop-exercise.js
+++ scripts/array-loop-exercise.js
@@ -82,3 +82,48 @@ function addOne(array) {
 
 console.log(addOne([1, 2, 3]));
 console.log(addOne([-2, -1, 0, 99]));
+
+function addNum(array, num) {
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i] + num)
+  }
+
+  return newArray;
+}
+
+console.log(addNum([1, 2, 3], 2));
+console.log(addNum([1, 2, 3], 3));
+console.log(addNum([-2, -1, 0, 99], 2));
+
+function addArrays(array1, array2) {
+  let doubledArray = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    doubledArray.push(array1[i] + array2[i]);
+
+  }
+
+  return doubledArray;
+}
+
+console.log(addArrays([1,1,2], [1,1,3]));
+console.log(addArrays([1,2,3], [4,5,6]));
+
+function countPositive(nums) {
+  let num = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    if (number > 0) {
+      num++
+    }
+  }
+
+  return num;
+}
+
+console.log(countPositive([1, -3, 5]));
+console.log(countPositive([-2, 3, -5, 7, 10]));
+
